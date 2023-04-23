9af8527fd67e52c3332473cb9a782e3862c8d42f
unknown <sudoxx2@gmail.com>
2023-04-23 10:35:44 -0700

    boilerplate crud

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+
+// Using Node.js `require()`
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+
+  console.log(req.body);
+  // res.send(req.body);
+});
+
+// update a product
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete a product
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:nani123@teacherlobbyapi.yc7jlo3.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected!"))
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(PORT, () => console.log(`its alive on httpL//localhost:${PORT}`));
